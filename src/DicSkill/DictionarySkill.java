94a8386d4cbd8faac2f0cd0dcdef3655cd61b38f
Pharmafairy <39585436+Pharmafairy@users.noreply.github.com>
2018-06-03 17:00:35 +0200

    Update 03.06.18
    Main class "DictionarySkill" will now load the context and settings upon starting up the programm.
    Saying "bye" will save the Context under "DictionarySkill/State/context_file.ser" and save the Settings under "DictionarySkill/State/settings_file.ser".
    This will have to be changed in the end stage, but will be useful for testing purposes.

--- src/DicSkill/DictionarySkill.java
+++ src/DicSkill/DictionarySkill.java
@@ -3,6 +3,17 @@ package DicSkill;
 import java.util.Scanner;
 
 /**
+ * 03.06.2018
+ * NEW:
+ * -	Class will now save and load settings and context
+ * TO DO:
+ * -	adapt, so the message may be received from the main system
+ * -	reconsider when context and settings shall be saved.
+ * @author Lia
+ *
+ */
+
+/**
  * 29.04.2018
  * This class is the one that can be run and will open a dialog with the user
  * say bye to close the dialog
@@ -15,8 +26,9 @@ public class DictionarySkill {
 	
 	public static void main(String[] args)
 	{
-		Context context = new Context();
-		Settings settings = new Settings();
+		State state = new State();
+		Context context = state.loadContext(); // loads context. If no context is found, creates new one.
+		Settings settings = state.loadSettings(); // loads settings. If no settings is found, creates new one.
 		MessageManager tb = new MessageManager();
 		DatabaseCommunicator dbC = new DatabaseCommunicator();
 		
@@ -34,6 +46,8 @@ public class DictionarySkill {
 			msg = read.nextLine();
 			msg.toLowerCase();
 		}
+		
+		state.save(settings, context); // saves the updated context
 		
 		System.out.println("Goodbye");
 	}
