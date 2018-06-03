dcfb3f83039e1b3bd7bd1dcc3e44e9f60ffd8637
Pharmafairy <39585436+Pharmafairy@users.noreply.github.com>
2018-06-03 16:56:56 +0200

    Update 03.06.2018
    State can now save and load context and settings

--- src/DicSkill/State.java
+++ src/DicSkill/State.java
@@ -1,5 +1,19 @@
 package DicSkill;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * 03.06.2018
+ * NEW:
+ * -	State can now save and load context and settings
+ * @author Lia
+ *
+ */
 
 /**
  * 29.04.2018
@@ -10,5 +24,92 @@ package DicSkill;
  */
 
 public class State {
+	
+	/** VARIABlES **/
+	File context_file;
+	File settings_file;
+	
+	
+	/** Constructor **/
+	State() {
+		context_file = new File("/State", "context_file.ser");
+		settings_file = new File("/State", "settings_file.ser");
+	}
+	
+	
+	/** Methods **/
+	public void save(Settings settings, Context context) {
 
+		try{
+			
+			//saves context
+			FileOutputStream fos = new FileOutputStream("context_file.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(context);
+			
+			//saves settings
+			fos = new FileOutputStream("settings_file.ser");
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(settings);
+			
+			oos.close();
+
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	// loads context file and returns context object
+	public Context loadContext() {
+		
+		Context context;
+			
+		try{
+			FileInputStream fis = new FileInputStream("context_file.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			context = (Context) ois.readObject();
+			ois.close();
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			return new Context();
+		}
+		catch(IOException e) {
+			System.out.println("New context has been created.");
+			return new Context();
+		}
+		
+		
+		return context;
+
+	}
+	
+	
+	// loads settings file and returns Settings object
+	public Settings loadSettings() {
+		
+		Settings settings;
+			
+		try{
+			FileInputStream fis = new FileInputStream("settings_file.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			settings = (Settings) ois.readObject();
+			ois.close();
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			return new Settings();
+		}
+		catch(IOException e) {
+			System.out.println("New settings have been created.");
+			return new Settings();
+		}
+		
+		return settings;
+
+	}
+
+	
 }
